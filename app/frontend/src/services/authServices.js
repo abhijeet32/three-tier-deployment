fb4f873820872251d67df0399d1c5762e69f5e99
abhijeet32 <kumarabhijeetips@gmail.com>
2026-02-05 18:43:29 +0530

    updated minikube url for accessing backend service

--- app/frontend/src/services/authServices.js
+++ app/frontend/src/services/authServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authUrl =
-    process.env.REACT_APP_AUTH_URL || "http://192.168.49.2:30080/api/auth";
+    process.env.REACT_APP_AUTH_URL || "http://127.0.0.1:57847/api/auth";
 
 console.log("Auth URL configured:", authUrl);
 
